5e0a7094a911f0fae52bb15aa0969c08a58a61a5
sa3d01 <sa3dsalem01@gmail.com>
2021-07-08 18:22:02 +0200

    api -- training courses

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -233,12 +233,12 @@ class UserController extends MasterController
         $count=count($request['type']);
         for ($i=0;$i<$count;$i++){
             $data['type']=$request['type'][$i];
-            $data['title']=$request['title'][$i];
-            $data['foundation_name']=$request['foundation_name'][$i];
-            $data['total_hours']=$request['total_hours'][$i];
-            $data['start_date']=$request['start_date'][$i];
-            $data['end_date']=$request['end_date'][$i];
-            $data['graduation_file']=$request['graduation_file'][$i];
+            $data['title']=$request['title']?$request['title'][$i]:'';
+            $data['foundation_name']=$request['foundation_name']?$request['foundation_name'][$i]:'';
+            $data['total_hours']=$request['total_hours']?$request['total_hours'][$i]:'';
+            $data['start_date']=$request['start_date']?$request['start_date'][$i]:'';
+            $data['end_date']=$request['end_date']?$request['end_date'][$i]:'';
+            $data['graduation_file']=$request['graduation_file']?$request['graduation_file'][$i]:'';
             $data['user_id']=$user->id;
             TrainingCourse::create($data);
         }
