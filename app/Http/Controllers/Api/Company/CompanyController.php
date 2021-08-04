6291cb524fb674b8e12b19abccdefb55241b5c28
sa3d01 <sa3dsalem01@gmail.com>
2021-08-05 00:30:00 +0200

    edits

--- app/Http/Controllers/Api/Company/CompanyController.php
+++ app/Http/Controllers/Api/Company/CompanyController.php
@@ -18,25 +18,39 @@ class CompanyController extends MasterController
     public function updateProfile(Request $request)
     {
         $company = auth('api')->user();
-        $user['phone']=$request['phone'];
-        $user['email']=$request['email'];
-        $user['country_id']=$request['country_id'];
-        $user['city_id']=$request['city_id'];
-        $user['major_id']=$request['major_id'];
-        $user['members_count']=$request['members_count'];
-        $user['avatar']=$request['avatar'];
-        $profile['working_type']=$request['working_type'];
-        $profile['cover']=$request['cover'];
-        $profile['location']=$request['location'];
-        $profile['commercial_file']=$request['commercial_file'];
-        $profile['foundation_name']=$request['foundation_name'];
-        $profile['description']=$request['description'];
+        $user['phone']=$request['phone']??$company->phone;
+        $user['email']=$request['email']??$company->email;
+        $user['country_id']=$request['country_id']??$company->country_id;
+        $user['city_id']=$request['city_id']??$company->city_id;
+        $user['major_id']=$request['major_id']??$company->major_id;
+        $user['members_count']=$request['members_count']??$company->members_count;
+        $company->update($user);
+        if ($request['avatar']){
+            $company->update([
+                'avatar'=>$request['avatar']
+            ]);
+        }
+        $profile['working_type']=$request['working_type']??$company->profile->working_type;
+        $profile['location']=$request['location']??$company->profile->location;
+        $profile['foundation_name']=$request['foundation_name']??$company->profile->foundation_name;
+        $profile['description']=$request['description']??$company->profile->description;
+        $company->profile->update($profile);
+        if ($request['cover']){
+            $company->profile->update([
+                'cover'=>$request['cover']
+            ]);
+        }
+        if ($request['commercial_file']){
+            $company->profile->update([
+                'commercial_file'=>$request['commercial_file']
+            ]);
+        }
+
+
         $socilas['facebook']=$request['facebook'];
         $socilas['twitter']=$request['twitter'];
         $socilas['youtube']=$request['youtube'];
         $socilas['linkedin']=$request['linkedin'];
-        $company->update($user);
-        $company->profile->update($profile);
         $social=Socials::where('user_id',$company->id)->first();
         if (!$social){
             $socilas['user_id']=$company->id;
