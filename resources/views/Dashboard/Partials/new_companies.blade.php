e8a3247c36deaf38318b899ebf62857551f087f3
sa3d01 <sa3dsalem01@gmail.com>
2021-08-15 13:29:03 +0200

    edits

--- resources/views/Dashboard/Partials/new_companies.blade.php
+++ resources/views/Dashboard/Partials/new_companies.blade.php
@@ -7,9 +7,9 @@
                         <img src="{{$company->avatar}}" class="img-fluid rounded-circle" alt="user">
                     </div>
                     <div class="wid-u-info">
-                        <h5 class="mt-0">{{$company->profile->foundation_name}}</h5>
+                        <h5 class="mt-0">{{$company->profile?$company->profile->foundation_name:""}}</h5>
                         <p class="text-muted mb-1 font-13 text-truncate">{{$company->email}}</p>
-                        <small class="text-warning"><b>{{$company->major->name_ar}}</b></small>
+                        <small class="text-warning"><b>{{$company->major?$company->major->name_ar:""}}</b></small>
                     </div>
                 </div>
             </div>
