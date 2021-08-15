96e4fedd2077981cf1c04d89da4044cb7a851f80
sa3d01 <sa3dsalem01@gmail.com>
2021-08-15 13:16:37 +0200

    edits

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => 'local',//env('APP_ENV', 'production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' =>true,// (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
