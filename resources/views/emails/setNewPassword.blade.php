5c27a354381485486e1b915927ca49d1a1f82ab7
sa3d01 <sa3dsalem01@gmail.com>
2021-11-01 17:54:01 +0200

    test mail

--- resources/views/emails/setNewPassword.blade.php
+++ resources/views/emails/setNewPassword.blade.php
@@ -4,7 +4,7 @@
     <title>Welcome Email</title>
 </head>
 <body>
-<h2>Welcome to bebaan {{$user['name']}}</h2>
+<h2>Welcome to bebaan </h2>
 <br/>
 Your registered email-id is {{$user['email']}} , Please click on the below link to set new password to your account
 <br/>
