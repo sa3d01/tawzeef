a7d0f6e31c46484e8d0d89d0ad936407e3cc42de
sa3d01 <sa3dsalem01@gmail.com>
2021-11-01 17:47:35 +0200

    test mail

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -82,8 +82,6 @@ class RegisterController extends MasterController
             'user_id' => $user->id,
             'token' => sha1(time())
         ]);
-        Mail::to($user->email)->send(new VerifyMail($user));
-
         try {
             Mail::to($user->email)->send(new VerifyMail($user));
         }catch (\Exception $e){
