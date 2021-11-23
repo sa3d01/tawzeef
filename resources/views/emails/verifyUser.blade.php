f43da89b78b7b952da5e47c61ba9c82010f9427f
sa3d01 <sa3dsalem01@gmail.com>
2021-11-23 19:27:10 +0200

    verify mail

--- resources/views/emails/verifyUser.blade.php
+++ resources/views/emails/verifyUser.blade.php
@@ -8,6 +8,6 @@
 <br/>
 Your registered email-id is {{$user['email']}} , Please click on the below link to verify your email account
 <br/>
-<a href="{{url('user/verify', $user->verifyUser->token)}}">Verify Email</a>
+your activation code is : {{$user->verifyUser->token}}
 </body>
 </html>
