5da819454d6a676252618e043cca255fbe2480c9
sa3d01 <sa3dsalem01@gmail.com>
2022-03-15 19:35:20 +0200

    user datatable

--- resources/views/Dashboard/user/index.blade.php
+++ resources/views/Dashboard/user/index.blade.php
@@ -12,7 +12,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card-box">
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
+                        <table id="" class="table table-striped table-bordered dt-responsive nowrap">
                             <thead>
                             <tr>
                                 <th>الإسم</th>
