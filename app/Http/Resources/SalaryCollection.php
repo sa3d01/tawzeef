0ff19c79984d34de4460d0ae5f32f54d64013725
sa3d01 <sa3dsalem01@gmail.com>
2022-06-25 19:00:31 +0200

    salary api

--- app/Http/Resources/SalaryCollection.php
+++ app/Http/Resources/SalaryCollection.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Job;
-use App\Models\User;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class SalaryCollection extends ResourceCollection
@@ -13,12 +11,12 @@ class SalaryCollection extends ResourceCollection
         $data = [];
         foreach ($this as $obj) {
             $arr['id'] = (int)$obj->id;
-            $arr['major'] = new MajorResourse($obj->major);
-            $arr['position']=$obj->position;
-            $arr['average_experience']=$obj->average_experience;
-            $arr['average_lowest_salary']=$obj->average_lowest_salary;
-            $arr['average_salary']=$obj->average_salary;
-            $arr['average_highest_salary']=$obj->average_highest_salary;
+            $arr['major'] = $obj->major_id;//new MajorResourse($obj->major);
+            $arr['position'] = $obj->position;
+            $arr['average_experience'] = $obj->average_experience;
+            $arr['average_lowest_salary'] = $obj->average_lowest_salary;
+            $arr['average_salary'] = $obj->average_salary;
+            $arr['average_highest_salary'] = $obj->average_highest_salary;
             $data[] = $arr;
         }
         return $data;
